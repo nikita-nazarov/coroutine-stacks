2086701f377132d6dceb0b06507dc177c090dd49
raehat <raehatsinghnanda@gmail.com>
2023-07-12 17:21:43 +0530

    Refactor updateCoroutineStackForest function

--- src/main/kotlin/com/nikitanazarov/coroutinestacks/CoroutineStacksPanel.kt
+++ src/main/kotlin/com/nikitanazarov/coroutinestacks/CoroutineStacksPanel.kt
@@ -4,77 +4,36 @@ import com.intellij.debugger.engine.*
 import com.intellij.debugger.engine.events.DebuggerCommandImpl
 import com.intellij.debugger.impl.DebuggerManagerListener
 import com.intellij.debugger.impl.DebuggerSession
+import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
-import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBPanelWithEmptyText
-import com.intellij.util.ui.JBUI
 import com.intellij.xdebugger.XDebuggerManager
 import org.jetbrains.kotlin.idea.debugger.coroutine.data.CoroutineInfoData
 import org.jetbrains.kotlin.idea.debugger.coroutine.data.State
 import org.jetbrains.kotlin.idea.debugger.coroutine.proxy.CoroutineDebugProbesProxy
 import java.awt.*
 import javax.swing.*
-import javax.swing.border.Border
 
 class CoroutineStacksPanel(project: Project) : JBPanelWithEmptyText() {
     private val panelContent = Box.createVerticalBox()
     private val forest = Box.createVerticalBox()
-
-    class CustomCellRenderer(
-        private val coroutinesActive: String?,
-        padding: Int = 3
-    ) : DefaultListCellRenderer() {
-        private val itemBorder = BorderFactory.createMatteBorder(0, 0, 1, 0, JBColor.GRAY)
-        private val leftPaddingBorder: Border = JBUI.Borders.emptyLeft(padding)
-        private val compoundBorder = BorderFactory.createCompoundBorder(itemBorder, leftPaddingBorder)
-
-        override fun getListCellRendererComponent(
-            list: JList<*>,
-            value: Any,
-            index: Int,
-            isSelected: Boolean,
-            cellHasFocus: Boolean
-        ): Component {
-            val renderer = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
-            if (renderer !is JComponent) {
-                return renderer
-            }
-
-            with(renderer) {
-                val listSize = list.model.size
-                if (index == 0) {
-                    toolTipText = coroutinesActive
-                    font = font.deriveFont(Font.BOLD)
-                } else if (index < listSize) {
-                    toolTipText = value.toString()
-                }
-                border = when {
-                    index < listSize - 1 -> compoundBorder
-                    index == listSize - 1 -> leftPaddingBorder
-                    else -> null
-                }
-            }
-
-            return renderer
-        }
-    }
-
 
     init {
         layout = BoxLayout(this, BoxLayout.Y_AXIS)
         emptyText.text = CoroutineStacksBundle.message("no.java.debug.process.is.running")
         val currentSession = XDebuggerManager.getInstance(project).currentSession
         if (currentSession != null) {
-            val currentProcess = (currentSession.debugProcess as JavaDebugProcess).debuggerSession.process
+            val javaDebugProcess = currentSession.debugProcess as? JavaDebugProcess
+            val currentProcess = javaDebugProcess?.debuggerSession?.process
             emptyText.component.isVisible = false
-            currentProcess.managerThread.invoke(object : DebuggerCommandImpl() {
+            currentProcess?.managerThread?.invoke(object : DebuggerCommandImpl() {
                 override fun action() {
                     buildCoroutineGraph(currentProcess.suspendManager.pausedContext)
                 }
             })
 
-            currentProcess.addDebugProcessListener(createPanelBuilderListener())
+            currentProcess?.addDebugProcessListener(panelBuilderListener())
         }
 
         project.messageBus.connect()
@@ -84,7 +43,7 @@ class CoroutineStacksPanel(project: Project) : JBPanelWithEmptyText() {
                 }
 
                 override fun sessionCreated(session: DebuggerSession) {
-                    session.process.addDebugProcessListener(createPanelBuilderListener())
+                    session.process.addDebugProcessListener(panelBuilderListener())
                 }
 
                 override fun sessionRemoved(session: DebuggerSession) {
@@ -95,7 +54,7 @@ class CoroutineStacksPanel(project: Project) : JBPanelWithEmptyText() {
             })
     }
 
-    private fun createPanelBuilderListener() = object : DebugProcessListener {
+    private fun panelBuilderListener() = object : DebugProcessListener {
         override fun paused(suspendContext: SuspendContext) {
             emptyText.component.isVisible = false
             buildCoroutineGraph(suspendContext)
@@ -126,7 +85,7 @@ class CoroutineStacksPanel(project: Project) : JBPanelWithEmptyText() {
         val dispatcherDropdownMenu = ComboBox(dispatchers)
 
         dispatcherDropdownMenu.addActionListener {
-            val selectedDispatcher = dispatcherDropdownMenu.selectedItem as String
+            val selectedDispatcher = dispatcherDropdownMenu.selectedItem as? String
             val coroutineDataList = dispatcherToCoroutineDataList[selectedDispatcher]
             if (!coroutineDataList.isNullOrEmpty()) {
                 updateCoroutineStackForest(coroutineDataList, suspendContextImpl)
@@ -164,16 +123,23 @@ class CoroutineStacksPanel(project: Project) : JBPanelWithEmptyText() {
         coroutineDataList: MutableList<CoroutineInfoData>,
         suspendContextImpl: SuspendContextImpl
     ) {
-        forest.removeAll()
-        val root = Node()
-        val coroutineStackForest = suspendContextImpl.buildCoroutineStackForest(
-            root,
-            coroutineDataList
-        ) ?: run {
-            emptyText.text = CoroutineStacksBundle.message("nothing.to.show")
-            return
-        }
-        forest.add(coroutineStackForest)
-        updateUI()
+        suspendContextImpl.debugProcess.managerThread.invoke(object : DebuggerCommandImpl() {
+            override fun action() {
+                val root = Node()
+                val coroutineStackForest = suspendContextImpl.buildCoroutineStackForest(
+                    root,
+                    coroutineDataList
+                ) ?: run {
+                    emptyText.text = CoroutineStacksBundle.message("nothing.to.show")
+                    return
+                }
+
+                runInEdt {
+                    forest.removeAll()
+                    forest.add(coroutineStackForest)
+                    updateUI()
+                }
+            }
+        })
     }
 }
